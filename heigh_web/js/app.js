ceee5dd8216c71dcde9806978d0cf3a0f9af1a26
yanghe <heyang04@gmail.com>
2016-12-09 21:06:52 +0800

    task12 Num2

---
+++ heigh_web/js/app.js
@@ -0,0 +1,139 @@
+$(document).ready(function() {
+    $(".title_1").click(function() {
+        $(".part_1").slideToggle();
+    });
+    $(".title_2").click(function() {
+        $(".part_2").slideToggle();
+    });
+    $(".title_3").click(function() {
+        $(".part_3").slideToggle();
+    });
+    $(".title_4").click(function() {
+        $(".part_4").slideToggle();
+    });
+    $(".title_5").click(function() {
+        $(".part_5").slideToggle();
+    });
+    $(".title_6").click(function() {
+        $(".part_6").slideToggle();
+    });
+    $(".title_7").click(function() {
+        $(".part_7").slideToggle();
+    });
+    $(".title_8").click(function() {
+        $(".part_8").slideToggle();
+    });
+    $(".title_9").click(function() {
+        $(".part_9").slideToggle();
+    });
+    $(".title_10").click(function() {
+        $(".part_10").slideToggle();
+    });
+    $(".title_11").click(function() {
+        $(".part_11").slideToggle();
+    });
+    $(".title_12").click(function() {
+        $(".part_12").slideToggle();
+    });
+    $(".title_13").click(function() {
+        $(".part_13").slideToggle();
+    });
+    $(".title_14").click(function() {
+        $(".part_14").slideToggle();
+    });
+    $(".title_15").click(function() {
+        $(".part_15").slideToggle();
+    });
+    $(".title_16").click(function() {
+        $(".part_16").slideToggle();
+    });
+    $(".title_17").click(function() {
+        $(".part_17").slideToggle();
+    });
+    $(".title_18").click(function() {
+        $(".part_18").slideToggle();
+    });
+    $(".title_19").click(function() {
+        $(".part_19").slideToggle();
+    });
+    $(".title_20").click(function() {
+        $(".part_20").slideToggle();
+    });
+    $(".title_21").click(function() {
+        $(".part_21").slideToggle();
+    });
+    $(".title_22").click(function() {
+        $(".part_22").slideToggle();
+    });
+    $(".title_23").click(function() {
+        $(".part_23").slideToggle();
+    });
+    $(".title_24").click(function() {
+        $(".part_24").slideToggle();
+    });
+    $(".title_25").click(function() {
+        $(".part_25").slideToggle();
+    });
+    $(".title_26").click(function() {
+        $(".part_26").slideToggle();
+    });
+    $(".title_27").click(function() {
+        $(".part_27").slideToggle();
+    });
+    $(".title_28").click(function() {
+        $(".part_28").slideToggle();
+    });
+    $(".title_29").click(function() {
+        $(".part_29").slideToggle();
+    });
+    $(".title_30").click(function() {
+        $(".part_30").slideToggle();
+    });
+    $(".title_31").click(function() {
+        $(".part_31").slideToggle();
+    });
+    $(".title_32").click(function() {
+        $(".part_32").slideToggle();
+    });
+    $(".title_33").click(function() {
+        $(".part_33").slideToggle();
+    });
+    $(".title_34").click(function() {
+        $(".part_34").slideToggle();
+    });
+    $(".title_35").click(function() {
+        $(".part_35").slideToggle();
+    });
+    $(".title_36").click(function() {
+        $(".part_36").slideToggle();
+    });
+    $(".title_37").click(function() {
+        $(".part_37").slideToggle();
+    });
+    $(".title_38").click(function() {
+        $(".part_38").slideToggle();
+    });
+    $(".title_39").click(function() {
+        $(".part_39").slideToggle();
+    });
+    $(".title_40").click(function() {
+        $(".part_40").slideToggle();
+    });
+    $(".title_41").click(function() {
+        $(".part_41").slideToggle();
+    });
+    $(".title_42").click(function() {
+        $(".part_42").slideToggle();
+    });
+    $(".title_43").click(function() {
+        $(".part_43").slideToggle();
+    });
+    $(".title_44").click(function() {
+        $(".part_44").slideToggle();
+    });
+    $(".title_45").click(function() {
+        $(".part_45").slideToggle();
+    });
+
+
+})
